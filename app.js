fea4a4774fc8b4342f6e6d14142a7fcac0e8609a
Martin Sookael <martinsookael@gmail.com>
2018-07-26 18:37:07 +0300

    added public folder and var v as container of all vues in frontend

--- app.js
+++ app.js
@@ -3,79 +3,111 @@ var http = require('http')
   , glob = require('glob')
   , _ = require('lodash')
   , fs = require('fs')
+  , path = require('path')
   , port = 5001
-  , vues = []
-  , html = '<html><body>'
 
-// collect location of all '.html' files in folder ./vues
+// COLLECT FRONT END DEPENCENCIES TO STRING deps
+var deps = ''
+var dependencies = [
+  '/js/lib/navigo.min.js',
+  '/js/lib/vue.min.js'
+]
+_.each(dependencies, function (oneDep) {
+  deps = deps + '<script src="' + oneDep + '"></script>\n'
+})
+// COLLECTS ADDRESSES OF ALL HTML FILES IN FOLDER './vues' TO ARRAY vues
+var vues = []
 glob('vues/*.html', function (err, files) {
   if (err) return console.log(err)
   vues = files
 })
+// COLLECT ADRESSES OF FILES IN PUBLIC FOLDER TO ARRAY publicFolder
+var publicFolder = []
+glob('public/**/*', function (err, publicFiles) {
+  if (err) return console.log(err)
+  publicFolder = publicFiles
+})
 
-// HTML header
-var header = `
-  <html>
-    <head>
-      <script src="//unpkg.com/navigo@6"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.16/vue.min.js"></script>
-
-      <script>
-        var router = new Navigo(null, true)
-        router.hooks({
-          before: function(done) {
-            // hide everything before every routing
-            world.show = false
-            hello.show = false
-            fourofour.show = false
-            done()
-          }
-        })
-        // handle 404
-        router.notFound(function () {
-          fourofour.show = true
-        })
-      </script>
-    </head>
-  <body>
-  <a href='#'>hello</a> | <a href='#/world'>world</a> | <a href='#/404'>404</a>
-`
-
-// Handle all incoming requests
+// HANDLE ALL INCOMING REQUESTS
 function handleServer (req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Request-Method', '*')
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token, X-Requested-With, Authorization')
-  // PREFLIGHT
+  // ANSWER PREFLIGHT REQUESTS
   if (req.method === 'OPTIONS') {
     res.writeHead(200)
     res.end()
   }
-  // ALL INCOMING GET REQUESTS, also the favicon 😠
+  // HANDLE ALL GET REQUESTS
   if (req.method === 'GET') {
-    var html2 = header
-    _.each(vues, function (oneVue) {
-      fs.readFile(oneVue, 'utf8', function (err, thisHTML) {
-        if (err) {
-          console.log(err)
-        } else {
-          html2 = html2 + thisHTML
-        }
-      })
-    })
-    setTimeout(function () {
+    var url = 'public' + req.url
+    if (publicFolder.indexOf(url) > -1) {
+      // HANDLE PUBLIC FOLDER CONTENTS
       res.writeHead(200)
-      html2 = html2 +
-        `<script>
-            router.resolve()
-          </script>
-        </body>
-      </html>`
-      res.write(html2)
+      fs.createReadStream(url).pipe(res)
+    } else if (req.url === '/') {
+      // HANDLE MAIN INDEX.HTML
+      var html = `
+        <html>
+          <head>
+            ` + deps + `
+            <script>
+              var v = {}
+              var router = new Navigo(null, true)
+              router.hooks({
+                before: function(done) {
+                  // hide everything before every routing
+                  for (var key in v) {
+                    if (v.hasOwnProperty(key) && v[key].show) {
+                      v[key].show = false
+                    }
+                  }
+                  done()
+                }
+              })
+              // handle 404
+              router.notFound(function () {
+                v.fourofour.show = true
+              })
+            </script>
+          </head>
+        <body>
+          <a href='#'>hello</a> | <a href='#/world'>world</a> | <a href='#/404'>404</a>
+        `
+        // INCLUDE CONTENTS FROM FILES IN FOLDER 'vues' TO html
+      _.each(vues, function (oneVue) {
+        fs.readFile(oneVue, 'utf8', function (err, thisHTML) {
+          if (err) {
+            console.log(err)
+          } else {
+            html = html + thisHTML
+          }
+        })
+      })
+      // WAIT 100 MS, PRINT THE FOOTER OF PAGE AND RESPOND
+      setTimeout(function () {
+        res.writeHead(200)
+        html = html +
+          `<script>
+              router.resolve()
+            </script>
+          </body>
+        </html>`
+        res.write(html)
+        res.end()
+      }, 10)
+    } else {
+      // HANDLE SERVER SIDE 404
+      res.writeHead(404)
+      res.write('not found - 404')
       res.end()
-    }, 100)
+    }
+  } else {
+    res.writeHead(501)
+    res.write('not implemented - 501')
+    res.end()
   }
 }
 
@@ -85,4 +117,4 @@ var server = http.createServer(function (req, res) {
 
 server.listen(port, function () {
   console.log('go to http://localhost:' + port)
-});
+})
